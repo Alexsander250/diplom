e6174fb6dbcadfdba176d8c4da27d58aa4765c7e
Alexander Mertsalov <43026480+Alexsander250@users.noreply.github.com>
2022-11-14 21:46:49 +0300

    Update controller.php

--- src/controller.php
+++ src/controller.php
@@ -4,29 +4,27 @@ namespace src;
 
 class Controller
     { 
-		public $shirinatable=0;
-		public $arr = array();
 		private $poster; 
-
 		function __construct() {
 			$this->poster = new POST();
 		}
-		
-
         public function menu()
         {		
-			$dbd = new database;	
 				
-			if (empty($this->poster->post_dep))
+			if (empty($this->poster->post_dep))	//если не выбран институт, то открывается страница для отладки
 			{
 				$menu = new Menu;				
 			}
-			switch ($this->poster->HTML_or_Excel){
+
+			$dbd = new database;
+			$service = new Service();
+
+			switch ($this->poster->HTML_or_Excel){	
 				case 1:
 					$html = new html;
 					break;
 				case 0:
-					if ($this->poster->post_dep!=7)
+					if ($this->poster->post_dep!=7)	//если выбран ВУЦ, то excel файл формируется отдельно (с остальными институтами выявляются ошибки (требует доработки))
 						{
 							$excel = new Excel;
 						}
