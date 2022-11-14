bb795696a0523383b84a1afc5ce9b22e169ced46
Alexander Mertsalov <43026480+Alexsander250@users.noreply.github.com>
2022-11-14 21:45:25 +0300

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -31,7 +31,8 @@ class Service
 		}    
     }
     
-    private function get_array_data_group_of_spec(){
+    private function get_array_data_group_of_spec(){						//формирование массива со специальностями и информацией о них, а также вложенный массив с группами и информации о них
+
         $result=$this->dbd->get_data_group_of_spec();
         //$result=$result->fetchAll();				
 				$i=0;
@@ -47,23 +48,23 @@ class Service
 				}
     }
 
-    public function list_array_groups(){
+    public function list_array_groups(){				//формирование массива с идентификаторами  и номерами групп (для начальной страцины (при внедрении не функция не обязательна))
         $result=$this->dbd->list_groups();
         foreach ($result as $row)
 			{
 				$this->arraygroups[$row['grid']]=$row['grnum'];
 			}
-            //print_r($this->arraygroups);			
+            
     }
 
-    private function get_array_predmets(){
+    private function get_array_predmets(){				//формирование вложенного массива с предметами изучаемых в группах (вкладывается во вложенный массив с группами)
         
         $result=$this->dbd->get_predmets($this->arraygroups);
         
-			foreach ($result as $row) 
+			foreach ($result as $row)					//обработка запроса на получение предметов 
 			{
-				if (!empty($this->array[$row['id']]['groups'][$row['grid']])) {
-					if (empty($this->poster->post_year)) {
+				if (!empty($this->array[$row['id']]['groups'][$row['grid']])) {		
+					if (empty($this->poster->post_year)) {		//проверка ввода данных о дате (если не введена, то расчет идет от нынешней даты), для вычисления интересуюжщего номера семестра в учебных планах групп		
                         $current_year=date("Y");
 						$sem = (($current_year - $this->array[$row['id']]['groups'][$row['grid']]['Fyear']) * 2) - $this->poster->post_sem;
 					} else {
@@ -74,10 +75,10 @@ class Service
 						}
 					}
 		
-					if ($row['SemNum'] === $sem) {
-
-						switch ($row['IDContr'])
-						{
+					if ($row['SemNum'] === $sem) {		//проверка соответсвия интересующего номера семестра и номера семестра в который проводилась аттестация по предмету 
+									
+						switch ($row['IDContr'])	
+						{						//распределение предметов, по типу контроля (формирование массива с типами контроля дисциплин и массива с идентификаторами дисциплин)
 							case 0:
 							case 1:
 								$this->array[$row['id']]['groups'][$row['grid']]['predmets']['exam'][$row['IDEPC']] = array(
@@ -113,7 +114,7 @@ class Service
     }
 
 
-    public function get_array_marks_students(){
+    public function get_array_marks_students(){			//формирование массива со списком студентов и их оценках по дисциплинами
         $result=$this->dbd->get_marks_students($this->arraygroups);
         foreach ($result as $row) {
             if (!empty($this->array[$row['id']]['groups'][$row['grid']]['predmets']['exam'][$row['IDEPC']]) ||
@@ -123,7 +124,7 @@ class Service
                 if ($row['grid'] == $row['Groupid']) {
                     $fio = $row['LastName'] . " " . $row['FirstName'] . " " . $row['MiddleName'] . " " . $row['FullNameDat'];
                     //echo '<br> arraybasic['.$row["id"].'][groups]['.$row["grid"].'][predmets]['.$row["IDContr"].']['.$row['IDEPC'].'][students]['.$fio.']'; 
-                    switch ((int)$row['IDContr'])
+                    switch ((int)$row['IDContr'])	
                     {
                         case 0:
                         case 1:
@@ -148,22 +149,24 @@ class Service
 
 
     
-		private function get_array_avg_marks_and_col_marks()
+		private function get_array_avg_marks_and_col_marks()	//получение средних оценок по дисциплинам и массивов с баллами оценивания
 		{
-			foreach ($this->array as $specID => $rowSpec) {
-				foreach ($rowSpec['groups'] as $id_group => $infGroup){
+			foreach ($this->array as $specID => $rowSpec) {		//прохождение основного массива по специальностям
+				foreach ($rowSpec['groups'] as $id_group => $infGroup){	//прохождение по группам
 					
-					if(!empty($infGroup['predmets']))
+					if(!empty($infGroup['predmets']))		
 					{			
 
-						foreach ($infGroup['predmets'] as $Typecontr => $predmet) {
-							foreach ($predmet as $IDEPC => $infpred) {
+						foreach ($infGroup['predmets'] as $Typecontr => $predmet) {	//прохождение по типам контроля дисциплин
+							foreach ($predmet as $IDEPC => $infpred) {	//прохождение по дисциплинам
 								//echo '<br> arraybasic['.$specID.'][groups]['.$id_group.'][predmets]['.$Typecontr.']['.$IDEPC.'][students]';
 								if(!empty($this->array[$specID]['groups'][$id_group]['predmets'][$Typecontr][$IDEPC]['students'])){
 								 
 									$marks=array_count_values($this->array[$specID]['groups'][$id_group]['predmets'][$Typecontr][$IDEPC]['students']);
 														
 									$summ=0;
+
+									//запись кол-ва оценок по дисциплине и среднего балла по дисциплине
 									switch ($infpred['type_cont']) {
 									case 0:
 									case 1:
@@ -209,15 +212,14 @@ class Service
 		}
 
 
-        private function max_kol_exam_and_zach()
+        private function max_kol_exam_and_zach()		//получение максимального кол-ва экзаменов, диф. зачетов и зачетов 
 		{			
 			$MaxKolExamOOP = 0;
 			$MaxKolZachetOOP = 0;
 			$zacmarks=0;
 			$difmarks=0;
-			//print_r($this->array);
-			foreach ($this->array as $specID => $rowSpec) {
-				foreach ($rowSpec['groups'] as $id_group => $infGroup) {
+			foreach ($this->array as $specID => $rowSpec) {	//прохождение по специальностям
+				foreach ($rowSpec['groups'] as $id_group => $infGroup) {	//прохождение по группам
 					$kolZachetOOP = 0;
 					$kolExamOOP = 0;
 					if(!empty($infGroup['predmets']['exam']))
@@ -246,19 +248,19 @@ class Service
 			$this->shirinatable = $shirinatable;
 		}
 
-		private function max_kol_exam_and_zach_VP()
+		private function max_kol_exam_and_zach_VP()		//получение максимольного кол-ва экзаменов и зачетов по военным дисциплинам 
 		{
 			$MaxKolExamVP = 0;
 			$MaxKolZachetVP = 0;
 
-			foreach ($this->array as $specID => $rowSpec) {
-				foreach ($rowSpec['groups'] as $id_group => $infGroup) {
+			foreach ($this->array as $specID => $rowSpec) {	//проождение по специальностям
+				foreach ($rowSpec['groups'] as $id_group => $infGroup) {	//прохождение по группам
 					$kolZachetVP = 0;
 					$kolExamVP = 0;
 					if (empty($infGroup['predmets']))
 					{echo '<br>'.$infGroup['grnum'];}
-					foreach ($infGroup['predmets'] as $Typecontr => $predmet) {			
-						foreach ($predmet as $IDEPC => $infpred) {
+					foreach ($infGroup['predmets'] as $Typecontr => $predmet) {	//прохождение по типам контроля дисциплины
+						foreach ($predmet as $IDEPC => $infpred) {	//прохождение по дисциплинам
 							if (!empty($this->array[$specID]['groups'][$id_group]['predmets'][$Typecontr][$IDEPC]) && $infpred['knum']==7 ) {
 								switch ($Typecontr)
 								{
@@ -280,29 +282,25 @@ class Service
 						$MaxKolZachetVP = $kolZachetVP;
 					}
 				}
-			}
-
-			
+			}			
 			$this->MaxKolExamVP=$MaxKolExamVP;
-			//$this->MaxKolExamOOP=$this->MaxKolExamOOP-$MaxKolExamVP;
 			$this->MaxKolZachetVP=$MaxKolZachetVP;
-			//$this->MaxKolZachetOOP=$this->MaxKolZachetOOP-$MaxKolZachetVP;
 			$this->shirinatable = $MaxKolExamVP+$MaxKolZachetVP+$this->MaxKolZachetOOP+$this->MaxKolExamOOP;
 		}
 
-		private function get_array_student_and_marks()
+		private function get_array_student_and_marks()		//получение массива студентов и их оценкам по дисциплине
 		{			
-					foreach ($this->array as $specID => $rowSpec) {
-						foreach ($rowSpec['groups'] as $id_group => $infGroup)
+					foreach ($this->array as $specID => $rowSpec) {	//прохождение по специальностям
+						foreach ($rowSpec['groups'] as $id_group => $infGroup)	//прохождение по группам
 						{
 							if(!empty($infGroup['predmets']))
 							{							
-								foreach ($infGroup['predmets'] as $Typecontr => $predmet){
+								foreach ($infGroup['predmets'] as $Typecontr => $predmet){	//прохождение по типам контроля дициплин
 									//print_r($infGroup);
-									foreach ($predmet as $IDEPC => $infpred) {
+									foreach ($predmet as $IDEPC => $infpred) {	//прохождение по дисциплинам
 										if(!empty($infGroup['predmets'][$Typecontr][$IDEPC]['students']))
 										{	
-											foreach ($infGroup['predmets'][$Typecontr][$IDEPC]['students'] as $fio => $mark) {
+											foreach ($infGroup['predmets'][$Typecontr][$IDEPC]['students'] as $fio => $mark) {	//формирование массива с оценками студентов по дисциплинам
 															
 												$this->arroc[$infGroup['grnum']][$fio][$Typecontr][$infpred['edu_name']] = array(
 												'knum' => $infpred['knum'],
@@ -314,8 +312,7 @@ class Service
 								}
 							}							
 						}
-					} 
-                    //print_r($this->arroc);
+					}                     
 		}
 
 
